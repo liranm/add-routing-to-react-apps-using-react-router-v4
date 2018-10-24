8f9a71c8c332cefad314cdf60f285ec55739f2aa
Liran Mery <liranm@ironsrc.com>
2018-10-24 17:40:18 +0300

    Style a Link that is Active with NavLink in React Router v4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 
+const isActive = (match, location) => {
+  console.log(match, location);
+  
+  return false;
+};
+
 const Links = () => (
   <nav>
-    <Link to="/">Home</Link>
-    <Link to={{ pathname: '/about' }}>About</Link>
-    <Link replace to="/contact">Contact</Link>
+    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+    <NavLink activeStyle={{ color: 'green' }} to={{ pathname: '/about' }}>About</NavLink>
+    <NavLink isActive={isActive} activeClassName="active" replace to="/contact">Contact</NavLink>
   </nav>
 );
 
@@ -16,8 +22,8 @@ const App = () => (
     <div>
       <Links />
       <Route exact path="/" render={() => <h1>Home</h1>} />
-      <Route exact path="/about" render={() => <h1>About</h1>} />
-      <Route exact path="/contact" render={() => <h1>Contact</h1>} />
+      <Route path="/about" render={() => <h1>About</h1>} />
+      <Route path="/contact" render={() => <h1>Contact</h1>} />
     </div>
   </Router>
 );
