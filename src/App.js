d71b3fe32506b3aca980cec4d584af3d4f2f7169
Liran Mery <liranm@ironsrc.com>
2018-10-25 17:45:00 +0300

    Redirect to Another Page with React Router v4

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom';
 
 import './App.css';
 
-const Home = () => (<h1>Home</h1>);
-const Menu = () => (
-  <div>
-    <h1>Menu</h1>
-    <Link to="/menu/food">Food</Link>
-    <Link to="/menu/drinks">Drinks</Link>
-    <Link to="/menu/sides">Sides</Link>
-    <Route 
-      path="/menu/:section" 
-      render={({ match }) => <h2>{match.params.section}</h2>}
-    />
-  </div>
+const loggedin = false;
+
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/old/123">Old</Link>
+    <Link to="/new/456">New</Link>
+    <Link to="/protected">Protected</Link>
+  </nav>
 );
 
 const App = (props) => (
   <Router>
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/menu">Menu</Link>
-      <Route exact path="/" component={Home} ></Route>
-      <Route path="/menu" component={Menu} ></Route>
+      <Links />
+      <Route exact path="/" render={() => (<h1>Home</h1>)} />
+      <Route 
+        path="/new/:str" 
+        render={({ match }) => (<h1>New: {match.params.str}</h1>)} 
+      />
+      <Route path="/old/:str" render={({ match }) => (
+        <Redirect to={`/new/${match.params.str}`} />
+      )} />
+      <Route path="/protected" render={() => (
+        loggedin ?
+          <h1>Welcome to the protected page</h1> :
+          <Redirect to={"/new/login"} />
+      )} />
     </div>
   </Router>
 );
