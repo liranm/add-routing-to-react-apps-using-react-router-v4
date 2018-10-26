bfbbedc18f0771bd7a79df6ce165f8dcac6d8d46
Liran Mery <liranm@ironsrc.com>
2018-10-26 06:21:46 +0300

    Intercept Route Changes with React Router v4 Prompt Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Prompt } from 'react-router-dom';
 
 import './App.css';
 
-const loggedin = false;
+const Home = () => (<h1>Home</h1>);
 
-const Links = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/old/123">Old</Link>
-    <Link to="/new/456">New</Link>
-    <Link to="/protected">Protected</Link>
-  </nav>
-);
+class Form extends React.Component {
+  state = {
+    dirty: false
+  }
+
+  setDirty = () => this.setState({ dirty : true })
+
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+        <input type="text" onInput={this.setDirty} />
+        <Prompt 
+          when={this.state.dirty} 
+          message="Data will be lost!" 
+        />
+      </div>
+    );
+  }
+}
 
 const App = (props) => (
   <Router>
     <div>
-      <Links />
-      <Route exact path="/" render={() => (<h1>Home</h1>)} />
-      <Route 
-        path="/new/:str" 
-        render={({ match }) => (<h1>New: {match.params.str}</h1>)} 
-      />
-      <Route path="/old/:str" render={({ match }) => (
-        <Redirect to={`/new/${match.params.str}`} />
-      )} />
-      <Route path="/protected" render={() => (
-        loggedin ?
-          <h1>Welcome to the protected page</h1> :
-          <Redirect to={"/new/login"} />
-      )} />
+      <Link to="/">Home</Link>
+      <Link to="/form">Form</Link>
+      <Route exact path="/" component={Home} />
+      <Route path="/form" component={Form} />
     </div>
   </Router>
 );
